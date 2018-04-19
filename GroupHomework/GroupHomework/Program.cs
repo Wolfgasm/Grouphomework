556f6a4f952ee0784f482ddf06168601eced92bd
DESKTOP-OAKDG8P\張澤瑜 <a106082016@mail.shu.edu.tw>
2018-04-19 12:32:14 +0800

    改為顯示人名

--- GroupHomework/GroupHomework/Program.cs
+++ GroupHomework/GroupHomework/Program.cs
@@ -13,14 +13,24 @@ namespace GroupHomework
 
 
             //建立一個空的List
-            List<int> number = new List<int>();
+            List<string> member = new List<string>();
+      
 
-            // 在List裡放入班上人數       
+            // = =......dafuq
+            string[] memberName = new string[] { "葉俊廷", "張澤瑜", "王程捷", "江儀婷", "張秉華", "陳柏霖", "黃紀瑜",
+                "黃昱維", "蔡逸群", "登琳", "世新魏鈞孝吧", "劉定南", "陳信如", "童信傑", "李岳倫", "鄒和恆", "簡毓玟", "劉子瑄", "蕭紹洋", "李亞宸la", "張以潔",
+                "邱仕紳", "呂家瑩", "世新林鑫佑", "徐均得", "陳佳欣", "張恩瑋", "周詮", "王湘婷", "蕭宇成", "Max Cheung(BANG)", "馬嘉誠Σヽ(ﾟД ﾟ; )", "陳昱嘉",
+                "康珈熏", "盧奕宏", "陳郁佳的大哥黃博涵", "遲正雯", "世新林星彤", "許晏誠ｘ勒是奇多　", "許子安", "高子晴", "陳思婷", "矝鵘鮿", "余彥廷",
+                "吳耀輝", "鄭丞智", "林冠廷", "世新何曜宇", "邵喬雨", "張豐愷", "林子晴", "邱雁回", "陳家欣是韓妞", "鄧雅馨", "謝汶珊", "李曼寧", "我是陳柏霖2號", "張鈺慈", "鍾宜珊", "raer_tsai",
+                "世新李姳諼", "鄭曼君", "陳柏霖", "世新許子安", "世新方若帆", "李曼寧", "世新張豐愷", "世新遲正雯", "世新周詮" };
+
+             //在List裡放入班上人名  
             for (int i = 0; i < 66; i++)
             {
-                number.Add(i);
+                member.Add(memberName[i]);
             }
 
+            //每跑完六個人名就利用WriteLine換行並增加顯示第幾組
             for (int a = 1; a <= 11; a++)
             {
 
@@ -30,24 +40,25 @@ namespace GroupHomework
                 for (int i = 0; i < 6; i++)
                 {
                     //隨機挑選一個數字放入r變數
-                    int r = rand.Next(1, number.Count);
+                    int r = rand.Next(1, member.Count);
 
-                    //嘗試印出number[r],如果有錯誤就離開迴圈,(不加這個一直說索引超出陣列之類的所以先偷懶用這招)
+
+                    //嘗試印出member[r]所存的內容,如果有錯誤就離開迴圈,(不加try catch會一直說索引超出陣列之類的,不會改所以先偷懶用這招)
                     try
                     {
-                        Console.Write(number[r] + " ");
+                        Console.Write(member[r] + "___");//底線是用來分隔人名比較好讀
                     }
                     catch { break; }
                     
 
-                    
-                    number.RemoveAt(r);
+
+                    member.RemoveAt(r);
                 }
                 Console.WriteLine("第" + a + "組");
             }
 
 
-            // 暫停
+            // 暫停畫面
             Console.Read();
 
         }
